b6bd06eb184ea4ec65cf5084bf8ec14e378e837c
denishdilshan <115126998+denishdilshan@users.noreply.github.com>
2025-09-05 07:04:11 +0530

    Add files via upload

---
+++ Practice_1.js
@@ -0,0 +1,16 @@
+// Java script  can be declared in 4 ways:
+// Modern JavaScript: Using let, Using const
+// Old JavaScript: Using var, Without using any keyword
+
+//Using let
+let x = 5;
+const y = 6;
+
+//Mixed Example
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+
+//Price1 and 2 declare in const keyword, constnat values cannot be changed
+//total declare in let keyword, let values can be changed
+
