4396eb854080aa24c442a2609c6ef0f2c97dc16e
denishdilshan <115126998+denishdilshan@users.noreply.github.com>
2025-09-08 06:20:16 +0530

    7thSep

--- Practice_1.js
+++ Practice_1.js
@@ -49,3 +49,88 @@ let carName = "Volvo";
 //&& logical and
 //|| logical or
 //! logical not
+
+
+
+////////////////////////
+//FUNCTIONS
+//Functionas are fundamental building blocks in JavaScript
+//It enable better code organization
+//Functions are reusable block of code 
+//'Functions execute when they are called
+
+function myName(price1, price2, p3){
+
+}
+
+//Arrow Functions
+//Before ARROW functions
+let myFunction = function(a, b) { return a * b};
+let result = myFunction(4, 3); // result is 12
+
+
+//After ARROW functions
+myFunction = (a, b) => a * b;
+
+let result2 = myFunction(4, 3); // result is 12
+document.getElementById("demo").innerHTML = "Results:" + result2
+
+
+//LOCAL VARIABLES 
+//Variables declared within a JavaScript function, become LOCAL to the function
+//Local variables can only be accessed from within the function.
+function goal(){
+    let careName = "Cobra";
+}
+
+//JavaScript variables have 3 types of scope:
+//1. Global Scope
+//2. Function Scope
+//3 . Block Scope
+
+
+//Global Scope
+//Variables declared outside the function, have Global Scope.
+//It can be access outside the function as well as inside the function
+
+//Function Scope
+//Variables declared inside the function are not accessible from outside the function
+function myFunction1() {
+    var BusName = "Audi";
+
+}
+
+//Block Scope
+//Variables declared inside a { } block cannot be accessed from outside the block
+
+
+//JavaScript Hoisting
+//In JavaScript, a variable can be declared after it has been used.
+//EXAMPLE 1
+b = 5; // Assign 5 to x
+
+elem = document.getElementById("demo"); // Find an element
+elem.innerHTML = x;
+
+var b; //Declare x
+
+
+//JavScript Objects
+//Object is a variable that can hold many variables.
+//The object have properties and methods
+//Objects should declared with const keyword
+//Create an Object
+const hero = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+};
+
+//We can create empty objective and add properties later
+const zero = {};
+
+//Add properties
+zero.name = "Helo1";
+zero.age = 50;
+zero.color = "red";
